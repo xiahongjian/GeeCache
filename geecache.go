259fed3df9ceb2e13f9205f21380f987dd1ec020
xiahongjian <hongjian.xia@qq.com>
2021-08-08 21:50:12 +0800

    添加测试代码

--- geecache.go
+++ geecache.go
@@ -1,11 +1,11 @@
 package geecache
 
 type Getter interface {
-	Get(key string) (ByteView, error)
+	Get(key string) ([]byte, error)
 }
 
-type GetterFunc func(key string) (ByteView, error)
+type GetterFunc func(key string) ([]byte, error)
 
-func (f GetterFunc) Get(key string) (ByteView, error) {
+func (f GetterFunc) Get(key string) ([]byte, error) {
 	return f(key)
 }

a5ee7f47a36cf8351220d8a2687bb495924f0125
xiahongjian <hongjian.xia@qq.com>
2021-08-11 00:30:44 +0800

    添加Group

--- geecache.go
+++ geecache.go
@@ -1,5 +1,7 @@
 package geecache
 
+import "sync"
+
 type Getter interface {
 	Get(key string) (ByteView, error)
 }
@@ -8,4 +10,34 @@ type GetterFunc func(key string) (ByteView, error)
 
 func (f GetterFunc) Get(key string) (ByteView, error) {
 	return f(key)
+}
+
+type Group struct {
+	name      string
+	getter    Getter
+	mainCache cache
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	mu.Lock()
+	defer mu.Unlock()
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheByte: cacheBytes},
+	}
+	groups[name] = g
+	return g
+}
+
+func GetGroup(name string) *Group {
+	mu.RLock()
+	g := groups[name]
+	mu.RUnlock()
+	return g
 }
